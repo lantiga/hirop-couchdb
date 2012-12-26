648eee3a1bab1e7eb080677d61afcea763cd0510
Luca Antiga <luca.antiga@orobix.com>
2012-12-26 18:52:03 +0100

    Couchdb backend now working

--- src/hirop_couchdb/core.clj
+++ src/hirop_couchdb/core.clj
@@ -7,46 +7,43 @@
             [cheshire.core :as json]
             [cemerick.url :as url]))
 
-(def ^:dynamic *connection-data* nil)
-
-(defn init [connection-data]
-  (alter-var-root #'*connection-data* (fn [_] connection-data))
-  (try
-    (clutch/get-database
-     (assoc (cemerick.url/url (:connection-string *connection-data*))
-       :username (:username *connection-data*)
-       :password (:password *connection-data*)))
-    (catch Exception e (prn e))))
-
 (defmacro with-db [& forms]
+  ;; TODO: if db not initialized, initialize it 
   `(clutch/with-db
      (assoc (cemerick.url/url (:connection-string *connection-data*))
        :username (:username *connection-data*)
        :password (:password *connection-data*))
      (do ~@forms)))
 
+(defn save-views
+  []
+  (clutch/save-view
+   "hirop"
+   (clutch/view-server-fns
+    :javascript
+    {:context
+     {:map
+      "function(doc) { if (doc['external-ids'] !== undefined) { emit(doc['external-ids'], doc); }}"}
+     :all
+     {:map
+      "function(doc) { if (doc.docs !== undefined) { for (var i=0; i<doc.docs.length; i++) { emit(doc.docs[i]._hirop.id, doc.docs[i]); }} else { emit(doc._id, doc) }}"}})))
+
+(defn init-database
+  [connection-data]
+  (try
+    (binding [*connection-data* connection-data]
+      (with-db
+        (clutch/get-database)
+        (save-views)))
+    (catch Exception e (prn e))))
+
 ;; Design:
 ;; entire contexts are stored in documents
-;; external documents (e.g. Tags) are stored separately with their ids
-;; and referenced in the contexts.
+;; external documents are stored separately with their ids and referenced in the contexts.
 ;; They could be stored in other contexts or as individual documents.
 ;; It's the same since we're going to get them through the view.
 
 (defn uuid [] (str (java.util.UUID/randomUUID)))
-
-(defn save-views
-  []
-  (with-db
-    (clutch/save-view
-     "docs"
-     (clutch/view-server-fns
-      :javascript
-      {:context
-       {:map
-        "function(doc) { if (doc['external-ids'] !== undefined) { emit(doc['external-ids'], doc); }}"}
-       :all
-       {:map
-        "function(doc) { if (doc.docs !== undefined) { for (var i=0; i<doc.docs.length; i++) { emit(doc.docs[i]._hirop.id, doc.docs[i]); }} else { emit(doc._id, doc) }}"}}))))
 
 (defn couch->hirop
   [doc]
@@ -60,46 +57,17 @@
      (hirop/assoc-hid (:_id doc))
      (hirop/assoc-hrev (:_rev doc)))))
 
-(defn get-docs-view-doc
+(defn get-hirop-view-doc
   [id]
   (->
-   (first (map :value (with-db (clutch/get-view "docs" :all {:key id}))))
+   (first (map :value (with-db (clutch/get-view "hirop" :all {:key id}))))
    couch->hirop))
 
-(defn get-docs-view-docs
+(defn get-hirop-view-docs
   [ids]
   (->>
-   (map :value (with-db (clutch/get-view "docs" :all {:keys ids})))
+   (map :value (with-db (clutch/get-view "hirop" :all {:keys ids})))
    (map couch->hirop)))
-
-(defn fetch*
-  [context]
-  (let [external-ids (:external-ids context)
-        context-doc-id (json/generate-string external-ids)
-        ;; Alternatively we could use the docs view
-        context-doc (with-db (clutch/get-document context-doc-id))
-        external-docs (map #(get-docs-view-doc %) (vals external-ids))
-        external-doctypes (set (hirop/get-external-doctypes context))
-        external-docs
-        (reduce
-         (fn [out rel-map]
-           (reduce
-            (fn [out [_ rel-ids]]
-              (if (coll? rel-ids)
-                (concat out (get-docs-view-docs rel-ids))
-                (conj out (get-docs-view-doc rel-ids))))
-            out
-            (filter #(contains? external-doctypes (first %)) rel-map)))
-         external-docs
-         (vals (:rels context-doc)))
-        external-docs (distinct external-docs)
-        context-doc (update-in context-doc [:docs] #(concat % external-docs))
-        docs
-        (map
-         (fn [doc]
-           (hirop/assoc-hrels doc (get-in context-doc [:rels (keyword (hirop/hid doc))])))
-         (:docs context-doc))]
-    docs))
 
 (defn- context-document-id-rev
   [doc]
@@ -111,6 +79,37 @@
   [context-doc-id context-doc-rev]
   (str context-doc-id "#" context-doc-rev))
 
+(defn fetch*
+  [context]
+  (let [external-ids (:external-ids context)
+        context-doc-id (json/generate-string external-ids)
+        context-doc (with-db (clutch/get-document context-doc-id))
+        context-doc-rev (:_rev context-doc)
+        external-docs (map #(get-hirop-view-doc %) (vals external-ids))
+        external-doctypes (set (hirop/get-external-doctypes context))
+        external-docs
+        (reduce
+         (fn [out rel-map]
+           (reduce
+            (fn [out [_ rel-ids]]
+              (if (coll? rel-ids)
+                (concat out (get-hirop-view-docs rel-ids))
+                (conj out (get-hirop-view-doc rel-ids))))
+            out
+            (filter #(contains? external-doctypes (first %)) rel-map)))
+         external-docs
+         (vals (:rels context-doc)))
+        external-docs (distinct external-docs)
+        context-doc (update-in context-doc [:docs] #(concat % external-docs))
+        docs
+        (map
+         (fn [doc]
+           (->
+            (if (:_rev doc) doc (hirop/assoc-hrev doc context-doc-rev))
+            (hirop/assoc-hrels (get-in context-doc [:rels (keyword (hirop/hid doc))]))))
+         (:docs context-doc))]
+    docs))
+
 ;; Eventually consider using CouchDB update handlers.
 (defn save*
   [context]
@@ -119,19 +118,13 @@
         docs (filter #(not (contains? external-doctypes (hirop/htype %))) docs)
         external-ids (:external-ids context)
         tmp-starred (filter hirop/has-temporary-id? (vals (:starred context)))
-        ;;uuids (:uuids (with-db (clutch/uuids (count tmp-starred))))
         uuids (repeatedly (count tmp-starred) uuid)
         tmp-map (zipmap (map hirop/hid tmp-starred) uuids)
-        context-doc-id (json/generate-string external-ids)
+        context-doc {:_id (json/generate-string external-ids)}
         context-doc
-        (if (with-db (clutch/document-exists? context-doc-id))
-          (with-db (clutch/get-document context-doc-id))
-          (with-db (clutch/put-document {:_id context-doc-id})))
-        ;; TODO IMPORTANT: here we should check the rev of the
-        ;; currently loaded context doc and make sure it's the same
-        ;; as the one that has been retrieved. If not return :conflict
-        ;; Also, the rev should then be incorporated in the context
-        ;; document, so that a 409 response can be triggered.
+        (if-let [rev (hirop/hrev (first (vals (:stored context))))]
+          (assoc context-doc :_rev rev)
+          context-doc)
         rels
         (reduce
          (fn [out doc]
@@ -171,14 +164,13 @@
          :external-ids external-ids
          :rels rels
          :docs docs}
-        context-doc (merge context-doc doc-data)
-        ]
+        context-doc (merge context-doc doc-data)]
     (try
+      ;; TODO: catch correct exception (on 409)
+      ;;  Analyze when the exception should be triggered
       (with-db
         (clutch/put-document context-doc))
       {:result :success :remap tmp-map}
-      ;; TODO: catch correct exception (on 409)
-      ;;  Analyze when the exception should be triggered
       (catch Exception e
         {:result :conflict}))))
 
