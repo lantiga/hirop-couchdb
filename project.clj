c09b4bdd69436381e42f4315b8501c1f1550e6ba
Luca Antiga <luca.antiga@orobix.com>
2013-01-22 22:40:12 +0100

    Bring revision back.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hirop-couchdb "0.1.2-SNAPSHOT"
+(defproject hirop-couchdb "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
