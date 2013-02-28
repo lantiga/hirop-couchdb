ebb0c099212f856ad26c125c0e5c6c12818b24c1
Luca Antiga <luca.antiga@orobix.com>
2013-02-28 12:17:20 +0100

    Updated README and project information

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject hirop-couchdb "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "CouchDB backend for hirop"
+  :url "https://github.com/orobix/hirop-couchdb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
